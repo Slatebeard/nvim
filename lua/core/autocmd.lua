4f1b3d717980e7bb6efa1027e239942f6379f4c3
Slatebeard <max.ekberg@iths.se>
2024-11-11 15:33:14 +0100

    fixing

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,15 +1,6 @@
 -- CUSTOM THINGS
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-
--- Close the initial empty buffer when starting Neovim if no files are open
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if #vim.api.nvim_list_bufs() == 1 and vim.bo[0].buftype == "" then
-      vim.cmd("bd")
-    end
-  end,
-})
 -- Highlight on yank
 --   See :help vim.highlight.on_yank()
 augroup("HighlightYank", { clear = true })
