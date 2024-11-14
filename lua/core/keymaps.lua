c3ed78efb96b943c87bb1b4e19c353ce0d1dfe5a
Slatebeard <max.ekberg@iths.se>
2024-11-14 14:29:45 +0100

    general work

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- For conciseness
-local opts = { noremap = true, silent = true }
+-- local opts = { noremap = true, silent = true }
 
 -- Disable the spacebar key's default behavior in Normal and Visual modes
 -- vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -102,5 +102,9 @@ vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = true, silent = false, des
 
 --CUSTOM KEYS
 vim.opt.laststatus = 0
-vim.api.nvim_set_keymap('n', '<C-M-b>', ':BuildJar<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Ctrl>b','BuildJar<CR>', { noremap = true, silent = true })
+
+
+
+
